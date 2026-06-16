18a8f0da413d06d41e0c1d78e04fa7873a5a59ee
Máximo Maidana <157309935+MaximoMaidana@users.noreply.github.com>
2026-06-16 04:55:20 +0000

    feat: crear modelo Permiso y modelo intermedio con integrante

---
+++ src/models/permiso.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    return sequelize.define('Permiso', {
+        id: {
+            type: DataTypes.UUIDV4,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+        },
+        fechaHoraInicio: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+            allowNull: false
+        },
+        fechaHoraFin: {
+            type: DataTypes.DATE,
+            allowNull: true
+        },
+        esAcceso: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true,
+            allowNull: false
+        }
+        
+    }, {
+        timestamps: true,
+        tableName: 'permisos'
+    });
+};
