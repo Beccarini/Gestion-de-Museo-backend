1fac25884e6e1e2407f08d6d1c75c6dbb3c6c39a
Máximo Maidana <157309935+MaximoMaidana@users.noreply.github.com>
2026-06-16 04:56:35 +0000

    feat: agregar relaciones many-to-many de proyecto y permiso

--- src/models/index.js
+++ src/models/index.js
@@ -14,12 +14,45 @@ const sequelize = new Sequelize({
 
 const Integrante = require('./integrante')(sequelize);  
 const Registro = require('./registro')(sequelize); 
+const Proyecto = require('./proyecto')(sequelize);
+const Permiso = require('./permiso')(sequelize);
+const IntegranteProyecto = require('./integranteProyecto')(sequelize);
+const IntegrantePermiso = require('./integrantePermiso')(sequelize);
 
 Integrante.hasMany(Registro, { foreignKey: 'integranteId', as: 'registros' });
 Registro.belongsTo(Integrante, { foreignKey: 'integranteId', as: 'integrante' });
 
+Integrante.belongsToMany(Proyecto, {
+  through: IntegranteProyecto,
+  foreignKey: 'integranteId',
+  otherKey: 'proyectoId',
+  as: 'proyectos'
+});
+Proyecto.belongsToMany(Integrante, {
+  through: IntegranteProyecto,
+  foreighKey: 'proyectoId',
+  otherKey: 'integranteId',
+  as: 'integrantes'
+});
+Integrante.belongsToMany(Permiso, {
+  through: IntegrantePermiso,
+  foreignKey: 'integranteId',
+  otherKey: 'permisoId',
+  as: 'permisos'
+});
+Permiso.belongsToMany(Integrante, {
+  through: IntegrantePermiso,
+  foreignKey: 'permisoId',
+  otherKey: 'integranteId',
+  as: 'integrantes'
+});
+
 module.exports = {
   sequelize,
   Integrante,
-  Registro
+  Registro,
+  Proyecto,
+  Permiso,
+  IntegranteProyecto,
+  IntegrantePermiso
 };
