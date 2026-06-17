30495c78a266315f8ba127ab4fb7e8c3209e873a
Martín <martincho5004@gmail.com>
2026-06-17 19:09:34 -0300

    feat: ruta de item

--- src/routes/item.js
+++ src/routes/item.js
@@ -3,17 +3,12 @@ const { body, param, validationResult } = require('express-validator');
 const { Op } = require('sequelize');
 const {Recurso, Cambio, Item}=require('../models');
 const router = express.Router();
-const validateDataCambio=[
+const validateDataItem=[
     body('cantidad')
         .isInt({min: 1}).withMessage('La cantidad no puede ser menor a 1'),
-    body('fecha')
+    body('estado')
         .trim()
-        .notEmpty().withMessage('La fecha es obligatoria')
-        .isISO8601().withMessage('El formato de fecha debe ser válido (AAAA-MM-DD)')
-        .toDate(),
-    body('razon')
-        .trim()
-        .notEmpty().withMessage('Ingrese la razón del cambio'),
+        .notEmpty().withMessage('El estado debe ser obligatorio'),
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
